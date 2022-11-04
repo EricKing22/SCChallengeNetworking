0659b689c40bed9be369d177e41ceab1d5377db4
EricKing22 <sd8g22@soton.ac.uk>
2022-11-04 22:00:15 +0000

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,13 +1,49 @@
 import java.io.IOException;
-import java.net.*;
-import  java.io.*;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
-public class Client {
-    public static void main(String[] args) throws IOException{
-        Socket s = new Socket("localhost", 4399);
+public class Client{
+    String username;
+    String in_message;
+    Socket s;
 
-        PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("Hello");
-        pr.flush();
+
+    public Client() throws IOException {
+        System.out.println("Welcome. Please enter your username:");
+        Scanner scanner = new Scanner(System.in);
+        this.username = scanner.nextLine();
+        this.in_message = "";
+        s = new Socket("localhost", 4399);
+    }
+
+    public void run(){
+        try{
+            while(true) {
+                PrintWriter printer = new PrintWriter(this.s.getOutputStream());
+                System.out.println("Input your message:");
+                Scanner scanner = new Scanner(System.in);
+                this.in_message = scanner.nextLine();
+                // If user wants to quit
+                if (in_message.equals("quit")){
+                    printer.println("<<[" + this.username + "] " + "has left>>");
+                    printer.close();
+                    break;
+                }
+
+                printer.println("[" + this.username + "]: " + this.in_message);
+                printer.flush();
+            }
+        }
+        catch (Exception e){
+
+        }
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Client client = new Client();
+        client.run();
     }
 }
