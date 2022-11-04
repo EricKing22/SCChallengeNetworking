5a060172d7a940af78d594a556cd74b3b2d6069f
EricKing22 <sd8g22@soton.ac.uk>
2022-11-04 21:59:19 +0000

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,18 +1,50 @@
+package week4;
+
+
 import java.io.IOException;
-import java.net.*;
-import  java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
-public class Server {
-    public static void main(String[] args) throws IOException{
-        ServerSocket ss = new ServerSocket(4399);
-        Socket s = ss.accept();
 
-        System.out.println("client connected");
+public class Server extends Thread{
 
-        InputStreamReader in = new InputStreamReader(s.getInputStream());
-        BufferedReader bf = new BufferedReader(in);
+    Socket socket;
+    ServerSocket serverSocket;
 
-        String line = bf.readLine();
-        System.out.println("Client" + line);
+
+    static ArrayList<Socket> sockets;
+
+    public Server() throws IOException {
+        serverSocket = new ServerSocket(4399);
+        sockets = new ArrayList<>();
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                socket = serverSocket.accept();
+                System.out.println("A new client is connected");
+                sockets.add(socket);
+                System.out.println("Number of activating users: " + sockets.size());
+                // Thread
+                Handler handler = new Handler(socket);
+                new Thread(handler).start();
+            } catch (Exception e) {
+
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = new Server();
+        server.start();
+
+
+
+
     }
 }
